9296af4aa469bb2f9355f0b8ec39449ff94204e0
Mohammad Fallah <mohammadfallah.w@gmail.com>
2026-06-02 18:45:26 +0330

    feat: implement protect route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,25 @@
-import { LogoutButton } from "@/components";
+import { LogoutButton, ThemeSwitcher } from "@/components";
+import { auth } from "@/lib/auth";
 import { LucideLogOut } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const DashboardPage = () => {
+const DashboardPage = async () => {
+  const user = await auth.api.getSession({ headers: await headers() });
+  if (!user) {
+    redirect("/login");
+  }
+
   return (
     <div className="container mx-auto px-4">
       <div className="flex items-center justify-between py-3">
         <h1 className="font-bold text-2xl">Authentication</h1>
-        <LogoutButton isIconOnly>
-          <LucideLogOut />
-        </LogoutButton>
+        <div className="flex gap-2">
+          <ThemeSwitcher />
+          <LogoutButton isIconOnly>
+            <LucideLogOut />
+          </LogoutButton>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { ThemeSwitcher } from "@/components";
-import { Button } from "@heroui/react";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const HomePage = () => {
-  return (
-    <div>
-      <Button>Click Me</Button>
-      <ThemeSwitcher />
-    </div>
-  );
+const HomePage = async () => {
+  const user = await auth.api.getSession({ headers: await headers() });
+
+  if (!user) {
+    redirect("/login");
+  } else {
+    redirect("/dashboard");
+  }
 };
 
 export default HomePage;
